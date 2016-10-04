8ba55f5b748be7d253faa55b9d7ff6db4c479905
shawnluxy <shawnluxy@gmail.com>
2016-10-03 21:47:48 -0400

    Login Page & Register Page HTML

---
+++ logic/index.js
@@ -0,0 +1,55 @@
+function main() {
+	$('#register').on("touchend", goRegister);
+	$('#log_in').on('touchend', login);
+}
+
+function goRegister() {
+	plus.webview.show("register", "slide-in-right");
+}
+
+function login() {
+    $('input').blur();
+    var user_account = {};
+    user_account.USER_ID = '0';
+    user_account.USER_NAME = $('#user_name').val();
+    user_account.USER_PASSWORD = $('#user_password').val();
+    if (!validateEmail(user_account.USER_NAME)) {
+        mui.alert("Please Enter A Correct Email Address");
+        return;
+    }
+    if (!validatePassword(user_account.USER_PASSWORD)) {
+        mui.alert("Please Enter a Password of Length 8-20 letters");
+        return;
+    }
+    plus.nativeUI.showWaiting();
+    $.post("http://159.203.4.199:8080/user_account/log_in", JSON.stringify(user_account), function (data) {
+        if (data === 'FAIL') {
+            mui.alert("Login Fail");
+        } else if (data === 'NO_SUCH_USER') {
+            mui.alert("User Not Existing");
+        } else if (data === 'WRONG_PASSWORD') {
+            mui.alert("Wrong Password");
+        } else {
+            mui.alert("Login Success!");
+        }
+        plus.nativeUI.closeWaiting();
+    }).error(function () {
+        mui.alert("Please Check Your Network Connection");
+        plus.nativeUI.closeWaiting();
+    });
+}
+
+function validateEmail(email) {
+    // First check if any value was actually set
+    if (email.length == 0) return false;
+    // Now validate the email format using Regex
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+    return re.test(email);
+}
+
+function validatePassword(password) {
+    if (password.length < 8 || password.length > 20) {
+        return false;
+    }
+    return true;
+}
---
+++ logic/register.js
@@ -0,0 +1,55 @@
+function main() {
+	$('#register').on('touchend', register);
+}
+
+function register() {
+	var username = $('#name').val();
+	var password = $('#password').val();
+	var re_password = $('#re_password').val();
+	if (!validateEmail(username)) {
+		mui.alert("Please Enter a Correct Email Address");
+		return;
+	}
+	if (!validatePassword(password)) {
+		mui.alert("Password Length: 8-20 letters");
+		return;
+	}
+	if (password !== re_password) {
+		mui.alert("Passwords Don't Match");
+		return;
+	}
+	var user = {
+		USER_NAME: username,
+		USER_PASSWORD: password,
+		id: '0'
+	};
+	plus.nativeUI.showWaiting();
+	$.post("http://159.203.4.199:8080/user_account/new_user", JSON.stringify(user), function (data) {
+        if (data === 'SUCCESS') {
+            mui.alert("Please Check Your Email Inbox");
+        } else if (data === 'THE_USER_NAME_ALREADY_EXISTED') {
+            mui.alert("User Existed");
+        } else if (data === 'FAIL') {
+            mui.alert("Register Fail");
+        }
+        plus.nativeUI.closeWaiting();
+    }).error(function () {
+        mui.alert('Please Check Your Network');
+        plus.nativeUI.closeWaiting();
+    });
+}
+
+function validateEmail(email) {
+    // First check if any value was actually set
+    if (email.length == 0) return false;
+    // Now validate the email format using Regex
+    var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/i;
+    return re.test(email);
+}
+
+function validatePassword(password) {
+    if (password.length < 8 || password.length > 20) {
+        return false;
+    }
+    return true;
+}

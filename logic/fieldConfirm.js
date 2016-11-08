4196849a551ed5124614c179ad35febfb2152d99
shawnluxy <shawnluxy@gmail.com>
2016-11-07 23:34:19 -0500

    update field Schedule

---
+++ logic/fieldConfirm.js
@@ -0,0 +1,13 @@
+function goBack() {
+    var webview = plus.webview.currentWebview();
+    webview.hide("pop-out");
+}
+
+function goPay() {
+    plus.webview.show("payPage", "pop-in");
+}
+
+function init() {
+    $('#goBack').on("touchend", goBack);
+    $('#confirm').on("touchend", goPay);
+}
--- logic/fieldSchedule.js
+++ logic/fieldSchedule.js
@@ -3,6 +3,7 @@ var field_type = [0,0,0];
 var Tables = [];
 var checkBox;
 var timeline, timefirst, timelast, timelen;
+var typebar3, typebar7, typebar11;
 var currentType3, currentType7, currentType11;
 var timebox_moved = false;
 var table_moved = false;
@@ -16,13 +17,13 @@ function goBack() {
 }
 
 function goConfirm() {
-//	var orders = getOrder();
-//  plus.webview.show("fieldConfirm", "pop-in");
+	var orders = getOrder();
+    plus.webview.show("fieldConfirm", "pop-in");
 }
 
 function pulldownRefresh() {
 	setTimeout(function(){
-//		getTime();
+		getTime();
 		mui("#pullrefresh").pullRefresh().endPulldownToRefresh();
 	},1000);
 }
@@ -39,8 +40,27 @@ function goCheckBox(e){
 	else if(sender.style.backgroundColor == 'rgb(255, 255, 255)'){
 		sender.style.backgroundColor = 'rgb(143, 195, 31)'; //green
 	}
-//	console.log(e.originalEvent.changedTouches[0].clientX);
-//	console.log(checkBox[2].getBoundingClientRect().left);
+	//
+	var click = e.originalEvent.changedTouches[0].clientX;
+	var left3 = checkBox[0].getBoundingClientRect().left;
+	var right3 = checkBox[0].getBoundingClientRect().right;
+	var left7 = checkBox[1].getBoundingClientRect().left;
+	var right7 = checkBox[1].getBoundingClientRect().right;
+	var left11 = checkBox[2].getBoundingClientRect().left;
+	var right11 = checkBox[2].getBoundingClientRect().right;
+	if(click>left3 && click<right3) {
+//		if (sender.style.backgroundColor == 'rgb(143, 195, 31)') {
+//			$("#price").innerHTML = "$" + 10;
+//		} else if(sender.style.backgroundColor == 'rgb(255, 255, 255)') {
+//			$("#price").innerHTML = "$" + 3;
+//		}
+	} else if(click>left7 && click<right7) {
+		
+	} else if(click>left11 && click<right11) {
+		
+	}
+//	console.log(click);
+//	console.log(left3);
 }
 function Handlers() {
 	var items = $(".os-time-box");
@@ -77,17 +97,20 @@ function setMove() {
 	timebox_moved = true;
 }
 
-function buildTypeBar() {
+function buildTypeBar(column3, column7, column11) {
 	var timeindex = localStorage.getItem("timebox_index");
-	var column3 = new mui.PopPicker();
+	//
 	var temp3 = [];
-	// var obj3 = {};
-	column3.setData([
-		{
-            value: '0',
-            text: 'Three 1'
-       	}
-	]);
+	if(field_info[0].length == 0) {
+		var obj3 = {value: '0', text: 'Null'};
+		temp3.push(obj3);
+	} else {
+		for(var i=0; i<field_info[0].length; i++) {
+			var obj3 = {value: i.toString(), text: 'Three '+(i+1)};
+			temp3.push(obj3);
+		}
+	}	
+	column3.setData(temp3);
 	$('#type3').on('touchend', function(){
 		column3.show(function(items){
 			currentType3 = parseInt(items[0].value);
@@ -95,13 +118,17 @@ function buildTypeBar() {
 		});
 	});
 	//
-	var column7 = new mui.PopPicker();
-	column7.setData([
-		{
-            value: '0',
-            text: 'Seven 1'
-       	}
-	]);
+	var temp7 = [];
+	if(field_info[1].length == 0) {
+		var obj7 = {value: '0', text: 'Null'};
+		temp7.push(obj7);
+	} else {
+		for(var i=0; i<field_info[1].length; i++) {
+			var obj7 = {value: i.toString(), text: 'Seven '+(i+1)};
+			temp7.push(obj7);
+		}
+	}
+	column7.setData(temp7);
 	$('#type7').on('touchend', function(){
 		column7.show(function(items){
 			currentType7 = parseInt(items[0].value);
@@ -109,13 +136,17 @@ function buildTypeBar() {
 		});
 	});
 	//
-	var column11 = new mui.PopPicker();
-	column11.setData([
-		{
-            value: '0',
-            text: 'Eleven 1'
-       	}
-	]);
+	var temp11 = [];
+	if(field_info[2].length == 0) {
+		var obj11 = {value: '0', text: 'Null'};
+		temp11.push(obj11);
+	} else {
+		for(var i=0; i<field_info[2].length; i++) {
+			var obj11 = {value: i.toString(), text: 'Eleven '+(i+1)};
+			temp11.push(obj11);
+		}
+	}
+	column11.setData(temp11);
 	$('#type11').on('touchend', function(){
 		column11.show(function(items){
 			currentType11 = parseInt(items[0].value);
@@ -146,107 +177,107 @@ function randomString(len) {
 
 function initTable(site_id) {
 	var Url = "http://159.203.4.199:8080/field/field/getFields/" + site_id.toString();
-	for(i=0; i<checkBox.length; i++) {checkBox[i].style.backgroundColor = 'rgb(255, 255, 255)';}
-//	for(i=0; i<checkBox.length; i++) {checkBox[i].style.backgroundColor = "gray";}
-//	field_info = [[],[],[]];
-//	field_type = [0,0,0];
-//	mui.ajax(Url, {
-//		type: "get",
-//		timeout: 10000,
-//		async: false,
-//		dataType: 'json',
-//      success: function (data) {
-//      		for(i=0; i<data.length; i++) {
-//				var index = parseInt(Math.sqrt(data[i].TYPE))-1;
-//				field_info[index].push(data[i]);
-//				field_type[index] += 1;
-//			}
-//      },
-//      	error: function (xhr, type) {
-//          alert(type);
-//      }
-//	});
-//	//  initialize table with available field type
-//	for(i=0; i<field_type.length; i++) {
-//		if(field_type[i] > 0) {
-//			for(j=0; j<timelen; j++) {checkBox[(field_type.length)*j+i].style.backgroundColor = 'rgb(255, 255, 255)';}
-//		}
-//	}
+	for(i=0; i<checkBox.length; i++) {checkBox[i].style.backgroundColor = "gray";}
+	field_info = [[],[],[]];
+	field_type = [0,0,0];
+	mui.ajax(Url, {
+		type: "get",
+		timeout: 10000,
+		async: false,
+		dataType: 'json',
+        success: function (data) {
+        		for(i=0; i<data.length; i++) {
+				var index = parseInt(Math.sqrt(data[i].TYPE))-1;
+				field_info[index].push(data[i]);
+				field_type[index] += 1;
+			}
+        },
+        	error: function (xhr, type) {
+            alert(type);
+        }
+	});
+	//  initialize table with available field type
+	for(i=0; i<field_type.length; i++) {
+		if(field_type[i] > 0) {
+			for(j=0; j<timelen; j++) {checkBox[(field_type.length)*j+i].style.backgroundColor = 'rgb(255, 255, 255)';}
+		}
+	}
+	buildTypeBar(typebar3, typebar7, typebar11);
 }
 
 function getTime() {
-//	var d = new Date();
-//	var table_3 = [], table_7 = [], table_11 = [];
-//	Tables = [];
-//	// init tables
-//	for(i=0; i<field_type.length; i++) {
-//		for(j=0; j<field_type[i]; j++) {
-//			if(i==0){
-//				table_3.push(init_week_table());}
-//			else if(i==1){
-//				table_7.push(init_week_table());}
-//			else if(i==2){
-//				table_11.push(init_week_table());}
-//		}
-//	}
-//	
-//	plus.nativeUI.showWaiting();
-//	for(var h=0; h<field_info.length; h++) {			// loop #field_type
-//		for(var l=0; l<field_info[h].length; l++) {
-//			var Url = "http://159.203.4.199:8080/field/field/getFieldTime/" + field_info[h][l].ID.toString();
-//			var hasparent = field_info[h][l].hasOwnProperty("PARENT_ID");
-//			mui.ajax(Url, {
-//				type: "get",
-//				timeout: 10000,
-//				async: false,
-//				dataType: 'json',
-//		        success: function (data) {
-//			        	for (var k=0 ;k<7; k++) {
-////			        		var Date = d.getFullYear() + "-" + months[k] + "-" + days[k];
-//						var Date = d.getFullYear() + "-09-12";
-//						for(var i=0; i<data.length; i++) {
-//							if(data[i].start.indexOf(Date) > -1) {
-//								var timeStart = data[i].start.split("-");	// 0:year 1:month 2:day 3:hour 4:minute
-//								var timeEnd = data[i].end.split("-");
-//								var start_index = (timeStart[3]-timefirst[0])*2 + parseInt((timeStart[4]-timefirst[1])/30);
-//								var end_index = (timeEnd[3]-timefirst[0])*2 + parseInt((timeEnd[4]-timefirst[1])/30);
-//								if((start_index>=0) && (end_index>0) && (start_index<timelen) && (end_index<=timelen)) {
-//									for(j=start_index; j<end_index; j++) {
-//										if(h==0) {
-//											table_3[l][k][j] += 1;
-//										} else if(h==1) {
-//											table_7[l][k][j] += 1;
-//											if(hasparent) {
-//												var parentid = field_info[h][l].PARENT_ID;
-//												var index = field_info[2].map(function(x){return x.ID;}).indexOf(parentid);
-//												table_11[index][k][j] += 0.5;
-//											}
-//										} else if(h==2) {
-//											table_11[l][k][j] += 1;
-//											var leftid = field_info[h][l].LEFT_ID;
-//											var rightid = field_info[h][l].RIGHT_ID;
-//											var lindex = field_info[1].map(function(x){return x.ID;}).indexOf(leftid);
-//											var rindex = field_info[1].map(function(x){return x.ID;}).indexOf(rightid);
-//											table_7[lindex][k][j] += 1;
-//											table_7[rindex][k][j] += 1;
-//										}
-//									}
-//								}
-//							}
-//						}
-//			        }
-//		        	},
-//		        	error: function (xhr, type) {
-//		            alert(type);
-//		        },
-//			});
-//		}
-//	}
-//	Tables.push(table_3);
-//	Tables.push(table_7);
-//	Tables.push(table_11);
-////	plus.nativeUI.closeWaiting();
-//	setTimeout(function(){plus.nativeUI.closeWaiting();},500);
+	var d = new Date();
+	var table_3 = [], table_7 = [], table_11 = [];
+	Tables = [];
+	// init tables
+	for(i=0; i<field_type.length; i++) {
+		for(j=0; j<field_type[i]; j++) {
+			if(i==0){
+				table_3.push(init_week_table());}
+			else if(i==1){
+				table_7.push(init_week_table());}
+			else if(i==2){
+				table_11.push(init_week_table());}
+		}
+	}
+	
+	plus.nativeUI.showWaiting();
+	for(var h=0; h<field_info.length; h++) {			// loop #field_type
+		for(var l=0; l<field_info[h].length; l++) {
+			var Url = "http://159.203.4.199:8080/field/field/getFieldTime/" + field_info[h][l].ID.toString();
+			var hasparent = field_info[h][l].hasOwnProperty("PARENT_ID");
+			mui.ajax(Url, {
+				type: "get",
+				timeout: 10000,
+				async: false,
+				dataType: 'json',
+		        success: function (data) {
+			        	for (var k=0 ;k<7; k++) {
+//			        		var Date = d.getFullYear() + "-" + months[k] + "-" + days[k];
+						var Date = d.getFullYear() + "-09-12";
+						for(var i=0; i<data.length; i++) {
+							if(data[i].start.indexOf(Date) > -1) {
+								var timeStart = data[i].start.split("-");	// 0:year 1:month 2:day 3:hour 4:minute
+								var timeEnd = data[i].end.split("-");
+								var start_index = (timeStart[3]-timefirst[0])*2 + parseInt((timeStart[4]-timefirst[1])/30);
+								var end_index = (timeEnd[3]-timefirst[0])*2 + parseInt((timeEnd[4]-timefirst[1])/30);
+								if((start_index>=0) && (end_index>0) && (start_index<timelen) && (end_index<=timelen)) {
+									for(j=start_index; j<end_index; j++) {
+										if(h==0) {
+											table_3[l][k][j] += 1;
+										} else if(h==1) {
+											table_7[l][k][j] += 1;
+											if(hasparent) {
+												var parentid = field_info[h][l].PARENT_ID;
+												var index = field_info[2].map(function(x){return x.ID;}).indexOf(parentid);
+												table_11[index][k][j] += 0.5;
+											}
+										} else if(h==2) {
+											table_11[l][k][j] += 1;
+											var leftid = field_info[h][l].LEFT_ID;
+											var rightid = field_info[h][l].RIGHT_ID;
+											var lindex = field_info[1].map(function(x){return x.ID;}).indexOf(leftid);
+											var rindex = field_info[1].map(function(x){return x.ID;}).indexOf(rightid);
+											table_7[lindex][k][j] += 1;
+											table_7[rindex][k][j] += 1;
+										}
+									}
+								}
+							}
+						}
+			        }
+		        	},
+		        	error: function (xhr, type) {
+		            alert(type);
+		        },
+			});
+		}
+	}
+	Tables.push(table_3);
+	Tables.push(table_7);
+	Tables.push(table_11);
+//	plus.nativeUI.closeWaiting();
+	setTimeout(function(){plus.nativeUI.closeWaiting();},500);
 }
 
 function getOrder() {
@@ -330,23 +361,26 @@ function init() {
 	timefirst = timeline[0].innerHTML.split(":");
 	timelast = timeline[timeline.length-1].innerHTML.split(":");
 	timelen = (timelast[0]-timefirst[0])*2 + parseInt((timelast[1]-timefirst[1])/30);
+	typebar3 = new mui.PopPicker();
+	typebar7 = new mui.PopPicker();
+	typebar11 = new mui.PopPicker();
     currentType3 = 0, currentType7 = 0, currentType11 = 0;
     Handlers();
 }
 
 function setTable(type,typeindex,timeindex) {
-//	var t = parseInt(Math.sqrt(type))-1;
-//	if(Tables[t].length > 0) {
-//		// change checkbox back to white
-//		for(i=0; i<timelen; i++) {
-//			checkBox[(field_type.length)*i+t].style.backgroundColor = 'rgb(255, 255, 255)';}
-//		// fill up the table
-//		for(j=0; j<timelen; j++) {
-//			if(Tables[t][typeindex][timeindex][j] > 0) {
-//				checkBox[(field_type.length)*j+t].style.backgroundColor = 'rgb(255, 204, 0)'; //orange
-//			}
-//		}
-//	}
+	var t = parseInt(Math.sqrt(type))-1;
+	if(Tables[t].length > 0) {
+		// change checkbox back to white
+		for(i=0; i<timelen; i++) {
+			checkBox[(field_type.length)*i+t].style.backgroundColor = 'rgb(255, 255, 255)';}
+		// fill up the table
+		for(j=0; j<timelen; j++) {
+			if(Tables[t][typeindex][timeindex][j] > 0) {
+				checkBox[(field_type.length)*j+t].style.backgroundColor = 'rgb(255, 204, 0)'; //orange
+			}
+		}
+	}
 }
 
 

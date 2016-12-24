847de17c41560d94da6724e5de1c64310a2cd8c3
Ringgold <ringgoldlin@yahoo.com>
2016-12-25 05:00:25 +0800

    Fix bugs of team main

--- logic/teamMain.js
+++ logic/teamMain.js
@@ -204,10 +204,7 @@ function drawChart3T(scored, lost){
 
 	var option = {
 	    tooltip : {
-	        trigger: 'axis',
-	        axisPointer : {            // 坐标轴指示器，坐标轴触发有效
-	            type : 'shadow'        // 默认为直线，可选为：'line' | 'shadow'
-	        }
+	        show: false,
 	    },
 	    legend: {
 	        show: false,

565445bd1280dfdc73806aa9667042c67f86602e
shawnluxy <shawnluxy@gmail.com>
2017-02-07 01:15:52 -0500

    integrate paypal API and page into UI

---
+++ logic/paypalListener.js
@@ -0,0 +1,35 @@
+
+function init() {
+    $('#goBack').on("touchend", goBack);
+}
+
+function goBack() {
+    var webview = plus.webview.currentWebview();
+    webview.hide("pop-out");
+}
+
+function setupPage() {
+	var paylink = getLink();
+	var subpage = plus.webview.getWebviewById('paylink');
+	subpage.loadURL(paylink);
+}
+
+function getLink() {
+	var plink = "";
+	Url = 'https://socceredge.info/api/paypal/payment/getPayUrlTest';
+	mui.ajax(Url, {
+		type: "get",
+		timeout: 10000,
+		async: false,
+        success: function (data) {
+        		plink = data;},
+        	error: function (xhr, type) {
+            alert(type);}
+	});
+	console.log(plink);
+	return plink;
+}
+
+function getFeedback() {
+	
+}
